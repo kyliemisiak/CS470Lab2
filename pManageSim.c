c891939fa2c18ba37a3bc7e90a791ce8bbfdaf0e
Kylianna <kyliemisiak@gmail.com>
2025-02-10 15:19:30 -0800

    Add file

---
+++ pManageSim.c
@@ -0,0 +1,11 @@
+// CS 470 - Lab 2
+// Demonstrates the use of fork(), wait(), and exec
+
+
+// Add necessary headers
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+echo "Hello Kylianna"
